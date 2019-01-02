c69c7d7fbeadfb79a867e7213b3c4a4de75186bb
abstract-animation-9914 <vm930@hotmail.com>
2019-01-02 13:03:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,15 +32,16 @@ def using_insert(array, element)
   array.insert(4,element)
 end
 
-#using_insert
-  takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array (FAILED - 14)
-  increases the length of the array (FAILED - 15)
+def using_uniq(array)
+  array.uniq()
+end
 
-#using_uniq
-  takes in an argument of an array and uses the uniq method to remove any duplicate items (FAILED - 16)
+def using_flatten(array)
+  array.flatten
+end
 
-#using_flatten
-  takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings (FAILED - 17)
+def using_delete()
+end
 
 #using_delete
   takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string (FAILED - 18)
